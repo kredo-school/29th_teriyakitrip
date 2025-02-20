0163b69831db4f300283a1e75ff314e3c91ed514
Sunao Kohazame <gubojiao.touroku+github@gmail.com>
2025-02-20 20:12:16 +0900

    Merge from main

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItinerariesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::group(['prefix' => 'itineraries', 'as' => 'itineraries.'], function() {
+        Route::get('/create_add', [ItinerariesController::class, 'show'])->name('show');
+        Route::get('/create', [ItinerariesController::class, 'create'])->name('create');
+        // Route::post('/store', [ItinerariesController::class, 'store'])->name('store');
+        // Route::delete('/{user_id}/destroy', [ItinerariesController::class, 'destroy'])->name('destroy');
+        // Route::get('/{user_id}/edit', [ItinerariesController::class, 'edit'])->name('edit');
+    });
+});
+
