d9f0572340d6cf0a2eaef7e85f8bb316e353a282
Naho-Y <na730474709@yahoo.co.jp>
2025-02-21 20:39:01 +0930

    fixing Controller

--- app/Http/Controllers/RestaurantReviewController.php
+++ app/Http/Controllers/RestaurantReviewController.php
@@ -34,9 +34,108 @@ class RestaurantReviewController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(RestaurantReview $restaurantReview)
+    public function show()
     {
-        //
+        // ダミーデータ
+        $restaurant = (object)[
+            'name' => 'Sample Restaurant',
+            'reviews' => [
+                (object)[
+                    'user' => (object)['name' => 'David.T', 'photo' => '/images/profiles/girl1.png'],
+                    'rating' => 4,
+                    'title' => 'What a surprise!',
+                    'body' => 'I had a great experience! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum velit fugit tenetur quo dolorem delectus assumenda sit quaerat expedita laborum odit vitae, commodi iusto quam enim tempore maiores corporis ea.',
+                    'photos' => [
+                        (object)['photo' => '/images/misonikomiudon.jpg'],
+                        (object)['photo' => '/images/kishimen.jpg'],
+                        (object)['photo' => '/images/wagashi2.jpg'],
+                    ],
+                    'itinerary' => (object)[
+                        'title' => '2025 Kyoto Trip',
+                        'photo' => '/images/itineraries/kyoto.jpg',
+                    ],
+                ],
+                (object)[
+                    'user' => (object)['name' => 'Emily.C','photo' => null],
+                    'rating' => 2,
+                    'title' => 'No Title',
+                    'body' => 'It took 20min to make coffee.',
+                    'photos' => [
+                        (object)['photo' => '/images/wagashi2.jpg'],
+                    ],
+                ],
+
+                (object)[
+                    'user' => (object)['name' => 'David.T', 'photo' => '/images/profiles/girl1.png'],
+                    'rating' => 4,
+                    'title' => 'What a surprise!',
+                    'body' => 'I had a great experience! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum velit fugit tenetur quo dolorem delectus assumenda sit quaerat expedita laborum odit vitae, commodi iusto quam enim tempore maiores corporis ea.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum velit fugit tenetur quo dolorem delectus assumenda sit quaerat expedita laborum odit vitae, commodi iusto quam enim tempore maiores corporis ea.',
+                    'photos' => [
+                        (object)['photo' => '/images/misonikomiudon.jpg'],
+                        (object)['photo' => '/images/kishimen.jpg'],
+                        (object)['photo' => '/images/wagashi2.jpg'],
+                    ],
+                    // David itinerary
+                    'itinerary' => (object)[
+                    'title' => '2025 Kyoto Trip',
+                    'photo' => '/images/itineraries/kyoto.jpg',
+                ],
+                    
+                ],
+                (object)[
+                    'user' => (object)['name' => 'Emily.C','photo' => null],
+                    'rating' => 2,
+                    'title' => 'No Title',
+                    'body' => 'It took 20min to make coffee.',
+                    'photos' => [
+                        (object)['photo' => '/images/wagashi2.jpg'],
+                    ],
+                    // Emily itinerary
+                    // 'itinerary' => (object)[
+                    // 'title' => 'Kyoto Exploration',
+                    // 'photo' => '/images/itineraries/kyoto2.jpg',
+                    // ],
+                ],
+                (object)[
+                    'user' => (object)['name' => 'David.T', 'photo' => '/images/profiles/girl1.png'],
+                    'rating' => 4,
+                    'title' => 'What a surprise!',
+                    'body' => 'I had a great experience!',
+                    'photos' => [
+                        (object)['photo' => '/images/misonikomiudon.jpg'],
+                        (object)['photo' => '/images/kishimen.jpg'],
+                        (object)['photo' => '/images/wagashi2.jpg'],
+                    ],
+                    // David itinerary
+                    'itinerary' => (object)[
+                    'title' => '2025 Kyoto Trip',
+                    'photo' => '/images/itineraries/kyoto.jpg',
+                ],
+                    
+                ],
+                
+                (object)[
+                    'user' => (object)['name' => 'David.T', 'photo' => '/images/profiles/girl1.png'],
+                    'rating' => 4,
+                    'title' => 'What a surprise!',
+                    'body' => 'I had a great experience!',
+                    'photos' => [
+                        (object)['photo' => '/images/misonikomiudon.jpg'],
+                        (object)['photo' => '/images/kishimen.jpg'],
+                        (object)['photo' => '/images/wagashi2.jpg'],
+                    ],
+                    // David itinerary
+                    'itinerary' => (object)[
+                    'title' => '2025 Kyoto Trip',
+                    'photo' => '/images/itineraries/kyoto.jpg',
+                ],
+                    
+                ],
+            ],
+        ];
+
+        // `reviews.show` ビューに `restaurant` データを渡して表示
+        return view('reviews.show')->with('restaurant', $restaurant);
     }
 
     /**
