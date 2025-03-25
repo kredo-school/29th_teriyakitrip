707d78d0001f70afdf76e8aeabbb4adb18342e61
Saki Nakata <clarinet.sakinko@gmail.com>
2025-03-25 22:00:00 +0900

    Displaying own itineraries in other user's mypage from link in toppage itineraries tab
    - add code in blade
    - modify controller

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -32,6 +32,7 @@ class MypageController extends Controller
     {
         // 他のユーザーのデータを取得
         $user = User::findOrFail($userId);
+        $itineraries = $user->itineraries()->where('is_public', 1)->latest()->limit(3)->get();
         $topRestaurantReviews = RestaurantReview::where('user_id', $userId)->latest()->take(3)->get();
         $restaurantReviews = RestaurantReview::where('user_id', $userId)->latest()->get();
 
@@ -42,7 +43,7 @@ class MypageController extends Controller
         }
 
         // 必要なデータをビューに渡す
-        return view('mypage.show_others', compact('user', 'topRestaurantReviews', 'restaurantReviews'));
+        return view('mypage.show_others', compact('user','itineraries', 'topRestaurantReviews', 'restaurantReviews'));
     }
 
 
